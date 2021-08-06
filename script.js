c0be84452feda8af652f3ab0de9ee51061c20a1e
Denis <logunovich@gmail.com>
2021-08-06 09:13:55 +0200

    version 1.1

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const text = document.querySelector('#my__text'),
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    const correctText = text.value.replace(/ "/gim, ' «').replace(/",/gim, '»,').replace(/" /gim, '» ').replace(/":/gim, '»:').replace(/";/gim, '»;').replace(/"\./gim, '».').replace(/Нм/gm, 'Н·м').replace(/кВтч/gim, 'кВт·ч').replace(/ -/gim, ' —').replace(/- /gim, '— ').replace(/ - /gim, ' — ').replace(/^"/gim, '«').replace(/"$/gim, '»').replace(/мвтч/gim, 'МВт·ч').replace(/  /gim, ' ').replace(/4х4/gim, '4×4').replace(/4x4/gim, '4×4').replace(/\$ /gim, '$').replace(/€ /gim, '€').replace(/л.с./gim, 'л. с.');
+    const correctText = text.value.replace(/ "/gim, ' «').replace(/",/gim, '»,').replace(/" /gim, '» ').replace(/":/gim, '»:').replace(/";/gim, '»;').replace(/"\./gim, '».').replace(/Нм/gm, 'Н·м').replace(/кВтч/gim, 'кВт·ч').replace(/ -/gim, ' —').replace(/- /gim, '— ').replace(/ - /gim, ' — ').replace(/^"/gim, '«').replace(/"$/gim, '»').replace(/"!/gim, '»!').replace(/"\?/gim, '»?').replace(/мвтч/gim, 'МВт·ч').replace(/  /gim, ' ').replace(/4х4/gim, '4×4').replace(/4x4/gim, '4×4').replace(/\$ /gim, '$').replace(/€ /gim, '€').replace(/л\.с\./gim, 'л. с.');
     correctTextField.textContent = correctText;
 });
